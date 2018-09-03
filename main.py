794afe49c91a912bdc9fe634d5de284c2ff839cb
nvonturk <nvonturk@gmail.com>
2018-09-03 13:42:49 -0500

    Cleaning raw data initial commit

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START app]
-import logging
-
 from google.appengine.api import urlfetch
 from google.cloud import storage
-import datetime
 from pytz import reference
-
 from flask import Flask, request
 
 import requests_toolbelt.adapters.appengine
 import json
-requests_toolbelt.adapters.appengine.monkeypatch()
-
+import logging
+import datetime
 
 app = Flask(__name__)
+
+requests_toolbelt.adapters.appengine.monkeypatch()
+
 storage_client = storage.Client()
 bucket_name = "crowd_source"
 bucket = storage_client.get_bucket(bucket_name)
@@ -40,7 +24,6 @@ def hello():
 @app.route('/predictit')
 def collect_market_data():
     is_cron = request.headers.get('X-Appengine-Cron', False)
-    print("````````````````````````")
     # if not is_cron:
     #     print("HUR")
     #     return 'Bad Request', 400
@@ -49,9 +32,9 @@ def collect_market_data():
         response = urlfetch.fetch("https://www.predictit.org/api/marketdata/all/", headers={"Accept": "application/json"})
         parsed = json.loads(response.content)
         blob_name = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S, " + localtime.tzname(datetime.datetime.now()))
+        blob_name = "raw/" + blob_name
         blob = bucket.blob(blob_name)
         blob.upload_from_string(response.content, content_type='application/json')
-        print("~~~~~~~~~~~~~~~~~~~~~~~~")
         return "Finished try", 200
     except Exception as e:
         logging.exception(e)
@@ -63,4 +46,3 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-# [END app]
--- test.py
+++ test.py
@@ -1,5 +1,36 @@
 from google.cloud import storage
+import json
+import pprint
 
+def gather_metadata(bucket, market):
+	stored_markets = bucket.list_blobs(prefix="metadata/")
+	if market['ID'] not in stored_markets:
+		pp = pprint.PrettyPrinter(indent=2)
+		contracts = market['Contracts']
+		del market['Contracts']
+		del market['Image']
+		storage_location = market['ID']
+		for contract in contracts:
+			pp.pprint(contract)
+
+	else:
+		metadata = bucket.blob(market['ID'])
+		print('here')
+
+# Define bucket/client where data is stored
 storage_client = storage.Client()
 bucket_name = "crowd_source"
 bucket = storage_client.get_bucket(bucket_name)
+
+# Pull list of "blobs" in the storage client and check to make sure that the metada has been stored
+new_blobs = bucket.list_blobs(prefix="raw/")
+
+for blob in new_blobs:
+	content = blob.download_as_string()
+	parsed = json.loads(content)
+	list_of_markets = parsed['Markets']
+	for market in list_of_markets:
+		gather_metadata(bucket, market)
+		break
+	break
+
